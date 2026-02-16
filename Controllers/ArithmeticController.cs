0270f0b82820fbff67d8806c92dcbc7b34a8bbab
Ankit Pramod Dubey <Adubey@QMTCORP.COM>
2026-02-16 18:57:57 +0530

    inial commit

---
+++ Controllers/ArithmeticController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorTest.Models;
+
+namespace CalculatorTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArithmeticController : ControllerBase
+    {
+
+        [HttpGet("calculate")]
+        public IActionResult ArithmeticCalculation()
+        {
+
+            decimal a = 10;
+            decimal b = 20;
+
+            Arithmetic result = new Arithmetic()
+            {
+                Add = a + b,
+                Sub = a - b,
+                Multiply = a * b,
+                Divide = b != 0 ? a / b : 0
+            };
+         
+
+            return Ok(result);
+
+        }
+    }
+}
---
+++ Models/Arithmetic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorTest.Models
+{
+    public class Arithmetic
+    {
+        public decimal? Add { get; set; }
+        public decimal? Sub { get; set; }
+        public decimal? Multiply { get; set; }
+        public decimal? Divide { get; set; }
+        public decimal? Total { get; set; }
+    }
+}
