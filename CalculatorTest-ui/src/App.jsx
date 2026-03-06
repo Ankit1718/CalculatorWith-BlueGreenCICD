12518e500cd88db148ecdd9da1fe7943f6389dbb
Ankit Pramod Dubey <Adubey@QMTCORP.COM>
2026-03-06 19:47:43 +0530

    Added React calculator UI

---
+++ CalculatorTest-ui/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import axios, { Axios } from "axios";
+
+function App() {
+
+  const [num1,setNum1] = useState("");
+  const [num2,setNum2] = useState("");
+  const [result,setResult] = useState("");
+
+  const calculateAdd = async () => {
+
+    const res= await axios.get(`https://localhost:44372/api/Arithmetic/calculate?a=${num1}&b=${num2}`);
+   
+    setResult(res.data.add);
+  };
+
+   const calculateSub = async() => {
+
+    const res = await axios.get(`https://localhost:44372/api/Arithmetic/calculate?a=${num1}&b=${num2}`);
+
+    setResult(res.data.sub);
+  };
+
+const clear=()=>{
+  setNum1('');
+  setNum2('');
+  setResult('');
+}
+
+  return (
+    <div>
+<div style={{border:"1px solid grey",borderRadius:"10px",marginLeft:"500px",width: "265px"}}>
+      <h2 style={{marginLeft:"50px"}}>Calculator</h2>
+
+      <input type="number" placeholder="Enter first number" style={{marginLeft:"35px"}}
+      value={num1} onChange={(e)=>setNum1(e.target.value)}/>
+
+      <br/><br/>
+
+      <input type="number"placeholder="Enter second number" style={{marginLeft:"35px"}}
+      value={num2} onChange={(e)=>setNum2(e.target.value)}/>
+
+      <br/><br/>
+
+      <button onClick={calculateAdd} style={{borderColor:"green",marginLeft:"4px"}} >Add</button>
+      <span style={{width:"20px" , display:"inline-block"}}></span>
+      <button onClick={calculateSub} style={{borderColor:"red"}}>Sub</button>
+       <span style={{width:"20px" , display:"inline-block"}}></span>
+      <button onClick={clear} style={{borderColor:"black"}}>Clear</button>
+
+
+      <h3 style={{marginLeft:"50px"}}>Result: {result}</h3>
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
